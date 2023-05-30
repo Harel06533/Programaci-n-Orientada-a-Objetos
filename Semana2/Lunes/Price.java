b8f050062465536c7c4e2c58dea0567d04e43ea5
= <harelolguin22@gmail.com>
2023-05-29 20:25:40 -0600

    added comments

--- Semana2/Lunes/Price.java
+++ Semana2/Lunes/Price.java
@@ -1,3 +1,5 @@
+/* Programa para calcular el precio con IVA (Basado en validaciones) */
+/* - Cada salto a la siguiente iteración suelta un mensaje específico basado en todas las posibles excepciones */
 package Semana2.Lunes;
 
 import javax.swing.JOptionPane;
@@ -5,31 +7,31 @@ import javax.swing.JOptionPane;
 public class Price {
   public static void main (String[] args) {
     double price = 0;
-    while (true) {
+    while (true) {                                                            //--> El loop iterará infinitas veces
       String stringPrice = paneInput("Enter price value");
-      if (stringPrice == null) {
+      if (stringPrice == null) {                                              //--> Si la entrada de texto fue null (tecla escape)
         paneMessage("Program Terminated", "EXIT");
-        return;
+        return;                                                               //--> Finaliza el método main
       }
 
-      if (stringPrice.isEmpty()) {
-        paneError("Empty input not valid");
-        continue;
+      if (stringPrice.isEmpty()) {                                            //--> Si es una string vacía (Fue la tecla Enter)
+        paneError("Empty input not valid");                       
+        continue;                                                             //--> Pasa a la siguiente iteración
       }
 
       try {
-        price = Double.parseDouble(stringPrice);
-        if (price <= 0) {
-          paneError("Negative or 0 numbers are not valid");
-          continue;
+        price = Double.parseDouble(stringPrice);                              //--> Intenta parsear el valor a un double
+        if (price <= 0) {                                                     //--> Si no regresó excepción pero sí un número negativo:
+          paneError("Negative or 0 numbers are not valid");         
+          continue;                                                           //--> Pasa a la siguiente iteración
         }
-      } catch (NumberFormatException e) {
+      } catch (NumberFormatException e) {                                     //--> Si saltó una excepción, pasa a la siguiente iteración
         paneError("Invalid input format");
         continue;
       }
-      break;
+      break;                                                                  //--> Si llegó hasta este punto, significa que la entrada fue válida y finaliza el loop
     }
-    price += (price * 0.16);
+    price += (price * 0.16);                                                  //--> Agrega IVA al precio
     paneMessage(String.format("$%.2f", price), "Price with IVA");
   }
 

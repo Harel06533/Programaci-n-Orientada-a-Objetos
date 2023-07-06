e6627359687644a63b5c302d13316396916ad4af
= <harelolguin22@gmail.com>
2023-07-06 11:54:29 -0600

    added heap and treeset

--- Semana7/Jueves/CompareTest.java
+++ Semana7/Jueves/CompareTest.java
@@ -1,15 +1,30 @@
 package Semana7.Jueves;
 
 import java.util.PriorityQueue;
+import java.util.TreeMap;
+import java.util.TreeSet;
 
 public class CompareTest {
   public static void main (String[] args) {
     PriorityQueue<Rectangle> rQueue = new PriorityQueue<>();
+    TreeMap<Integer, Rectangle> rMap = new TreeMap<>();
+    TreeSet<Rectangle> rSet = new TreeSet<>();
 
     for (int i = 0; i < 10; i++) {
-      rQueue.add(new Rectangle((Math.random() * 100) + 1, (Math.random() * 100) + 1));
+      int randBase = getRandomNumber(1, 100);
+      int randHeight = getRandomNumber(1, 100);
+
+      rQueue.add(new Rectangle(randBase, randHeight));
+      rMap.put(i, new Rectangle(randBase, randHeight));
+      rSet.add(new Rectangle(randBase, randHeight));
     }
 
-    System.out.println(rQueue);
+    System.out.println("HEAP:\n" + rQueue);
+    System.out.println("MAP:\n" + rMap);
+    System.out.println("SET:\n" + rSet);
   } 
+
+  public static int getRandomNumber (int min, int max) {
+    return (int)((Math.random() * max) + min);
+  }
 }
